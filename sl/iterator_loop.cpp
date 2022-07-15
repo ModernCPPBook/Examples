2bc7c59005c87e91775cbffc57e2fba87207364b
Patrick <patrickdiehl@lsu.edu>
2022-07-15 16:38:44 -0500

    Update

---
+++ sl/iterator_loop.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include <list>
+
+int main(void) {
+  std::list<int> values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  // Accessing the iterator to the first element
+  std::list<int>::iterator it = std::begin(values);
+
+  for (; it != std::end(values); it++)
+    // Accesisng the elment using the dereference operator *
+    std::cout << *it << std::endl;
+
+  std::cout << "------" << std::endl;
+
+  for (int value : values) std::cout << value << std::endl;
+
+  return EXIT_SUCCESS;
+}
