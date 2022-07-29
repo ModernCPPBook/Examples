95fc485464f4d3dc960775b153b25762aa674af6
Patrick <patrickdiehl@lsu.edu>
2022-07-29 17:08:06 -0500

    Add async for loop in HPX

---
+++ hpx/parallel_for_async.cpp
@@ -0,0 +1,21 @@
+#include <array>
+#include <cmath>
+#include <cstdlib>
+#include <hpx/algorithms.hpp>
+#include <hpx/hpx_main.hpp>
+#include <hpx/numeric.hpp>
+
+int main(int args, char** argv) {
+  std::array<int, 100000> a;
+
+  hpx::future<void> f = hpx::for_loop(hpx::execution::par(hpx::execution::task),
+                                      0, a.size(), [&](size_t i) {
+                                        a[i] = i * i;
+                                      });
+
+  f.then([a](auto&& f) {
+    std::cout << hpx::reduce(std::begin(a), std::end(a), 0) << std::endl;
+  });
+
+  return EXIT_SUCCESS;
+}
