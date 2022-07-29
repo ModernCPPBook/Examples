f317e4d331d48720498dd99adfdf49a86dd8dd16
Patrick <patrickdiehl@lsu.edu>
2022-07-29 15:57:41 -0500

    Add HPX Taylor example

---
+++ hpx/taylor_hpx.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/algorithms.hpp>
+#include <hpx/numeric.hpp>
+
+int main(int args, char** argv) {
+  int n = std::stoi(argv[1]);
+  double x = std::stod(argv[2]);
+
+  std::vector<double> parts(n);
+  std::iota(parts.begin(), parts.end(), 1);
+
+  hpx::for_each(hpx::execution::par,parts.begin(), parts.end(), [x](double& e) {
+    e = std::pow(-1.0, e + 1) * std::pow(x, e) / (e);
+  });
+
+  double result = hpx::reduce(hpx::execution::par,parts.begin(), parts.end(), 0.);
+
+  std::cout << "Difference of Taylor and C++ result "
+            << result - std::log(1 + x) << " after " << n << " iterations."
+            << std::endl;
+  return EXIT_SUCCESS;
+}
