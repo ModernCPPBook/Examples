efe02971692c719133cde823a88d0255cf971e30
Patrick Diehl <patrickdiehl@lsu.edu>
2022-08-19 15:36:14 -0500

    Add simd example

---
+++ hpx/parallel_reduce_simd.cpp
@@ -0,0 +1,15 @@
+#include <array>
+#include <cmath>
+#include <cstdlib>
+#include <hpx/hpx_main.hpp>
+#include <hpx/numeric.hpp>
+#include <hpx/parallel/algorithm.hpp>
+#include <hpx/include/datapar.hpp>
+
+int main(int args, char** argv) {
+  std::array<int, 100000> a;
+
+  std::cout << hpx::reduce(hpx::execution::par_simd, std::begin(a), std::end(a), 0) << std::endl;
+
+  return EXIT_SUCCESS;
+}
