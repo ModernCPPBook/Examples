0e0eefd0d3144d893fb7b8d1b71845c6aa436a03
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-11-30 09:07:48 -0600

    Update run_all.sh script

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,11 @@
+export NUM_THREADS=$(lscpu | grep '^CPU(s):'|cut -d: -f2|sed 's/\s//g')
+echo "Setting NUM_THREADS to $NUM_THREADS"
 cd ./build/set/
 for p in ./*
 do
 	if [ ! -d $p -a -x $p ]
 	then
 		TIME=$($p|tail -1)
-		echo $p $TIME
+		echo $TIME $p
 	fi
-done
+done | sort -n
